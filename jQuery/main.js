1943dfe932f7321efe9c708b892d35d1b193e802
subthings <Annzaychikova@gmail.com>
2019-08-04 23:36:14 +0700

    Final

--- jQuery/main.js
+++ jQuery/main.js
@@ -1,16 +1,24 @@
+$('.spinner-border').hide();
+
 $('button').click(function(event) {
     let data;
 
+    let numPool = [ 1, 3, 5, 7, 9, 10 ],
+        rand = numPool[Math.floor(Math.random() * numPool.length)];
+    $('button').hide();
+    $('.spinner-border').show();
     $.ajax({
         method: 'GET',
-        url: 'https://swapi.co/api/films/1/',
+        url: 'https://swapi.co/api/films/'+rand,
         dataType: 'json',
     }).done((response) => {
         console.log(response);
-        $.map(response => {
-            $('result').append('<h3>+result.director+</h3>')
-            }
-        )
+        $('button').show();
+        $('.spinner-border').hide();
+        $('#inputTitle').attr("placeholder", response.title);
+        $('#inputDirector').attr("placeholder", response.director);
+        $('#inputProducer').attr("placeholder", response.producer);
+        $('#inputReleaseDate').attr("placeholder", response.release_date);
     })
         .fail((status) => {
             console.log(status)
