bc32314f9271662acada5799cf31276982ffa97a
subthings <Annzaychikova@gmail.com>
2019-08-04 23:05:05 +0700

    Got response

--- jQuery/main.js
+++ jQuery/main.js
@@ -1,16 +1,19 @@
-/*
-$.ajax({
-    type: 'GET',
-    url: 'http https://swapi.co/api/films/1/',
-    data: {name: 'Ivan', age: 15},
-    dataType: 'json'
-}).done ((response) => {
-    console.log(response)
-}).fail((jqXHR, textStatus) => {
-    console.log(status)
-});
-*/
+$('button').click(function(event) {
+    let data;
 
-$(document).ready(function () {
-    $('body div').css('border', 'solid 3px blue');
+    $.ajax({
+        method: 'GET',
+        url: 'https://swapi.co/api/films/1/',
+        dataType: 'json',
+    }).done((response) => {
+        console.log(response);
+        $.map(response => {
+            $('result').append('<h3>+result.director+</h3>')
+            }
+        )
+    })
+        .fail((status) => {
+            console.log(status)
+        });
+
 });
